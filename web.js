965170eddf8847fc514959eb649a4c88293786d0
Al Gamboa <alphonse.gamboa@gmx.net>
2013-07-16 04:30:05 +0200

    Added website layout files

---
+++ web.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var mesg = fs.readFileSync("index.html", "utf8");
+
+app.get('/', function(request, response) {
+  response.send(mesg);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
